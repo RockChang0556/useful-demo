890d160028fce08246b349cd0419e668899cf5fd
RockChang0556 <RockChang0556@users.noreply.github.com>
2024-05-26 02:59:27 +0000

    Deploying to gh-pages from @ RockChang0556/useful-demo@82cdebc808a9a9b833cb61fe4882366d1f73fe19 🚀

---
+++ assets/index-83cbf496.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./header.vue_vue_type_style_index_0_lang-a0501a08.js";import{d as _,G as n,o as r,f as s,O as e}from"./index-ead78b8e.js";const a={class:"errpage-wrap"},l=_({__name:"index",setup(c){return(p,i)=>{const o=n("router-view");return r(),s("div",a,[e(t),e(o)])}}});export{l as default};
