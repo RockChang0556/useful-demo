580cbb40c9a363891ad72ce193d73b134b1d0b0a
RockChang0556 <RockChang0556@users.noreply.github.com>
2024-07-20 09:05:39 +0000

    Deploying to gh-pages from @ RockChang0556/useful-demo@3e14563c145ebd0784fd0e471048d0e48f46dacb 🚀

---
+++ assets/delayAnimation-172a7634.js
@@ -0,0 +1,7 @@
+import"./base-93c65733.js";import{E as m}from"./el-input-number-64bd073e.js";import"./el-input-17717aba.js";import"./el-popper-b288e4d8.js";import{d as p,k as n,G as c,o as u,f as v,O as a,H as o}from"./index-fae2b5f1.js";import"./use-form-item-93d94c50.js";import"./index-83314af5.js";import"./_baseGet-5f66532d.js";import"./index-513190a3.js";const f={class:"content"},y={class:"cssDelayAnimation"},h={class:"left"},V=o("span",null,null,-1),x=[V],D=p({__name:"delayAnimation",setup(k){const t=n(),s=n(0),i=l=>{var e;(e=t.value)==null||e.style.setProperty("--delay",`-${l}s`)};return(l,e)=>{const r=c("v-md-preview"),_=m;return u(),v("div",f,[a(r,{text:`
+> 通过进度条控制动画时间轴, 来改变动画位置, 从而实现动画随着进度条改变的效果
+
+::: tip
+- 动画时间轴
+:::
+`}),o("div",y,[o("div",h,[o("div",{class:"roll",ref_key:"rollRef",ref:t},x,512),a(_,{modelValue:s.value,"onUpdate:modelValue":e[0]||(e[0]=d=>s.value=d),min:0,max:1,step:.01,"show-tooltip":!1,onInput:i},null,8,["modelValue"])])])])}}});export{D as default};
